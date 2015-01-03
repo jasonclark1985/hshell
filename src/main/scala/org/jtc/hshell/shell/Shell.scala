b798718c4741fa3bcb855caa41e49ad8ca0c8e5c
Jason Clark <jason.clark.03@gmail.com>
2015-01-03 16:49:08 -0600

    refactoring

--- src/main/scala/org/jtc/hshell/shell/Shell.scala
+++ src/main/scala/org/jtc/hshell/shell/Shell.scala
@@ -29,10 +29,16 @@ class Shell {
   val parseCommand = new ParseCommand
   val localFilesystem = new LocalFilesystem
   val hdfs = new HadoopFilesystem
-  val prompt = new Prompt(shouldExit = { s: String => s.startsWith("exit") })
-  val lpwd: PartialFunction[Command, Option[String]] = { case c:Command if c.command == "lpwd" => Option(localFilesystem.workingDirectory) }
-  val lls: PartialFunction[Command, Option[String]] = { case c:Command if c.command == "lls" => Option(localFilesystem.list(c.arguments.headOption)) }
-  val lcd: PartialFunction[Command, Option[String]] = { case c:Command if c.command == "lcd" => localFilesystem.changeWorkingDirectory(c.arguments.head) ; None }
+  val prompt = new Prompt(shouldExit = { s: String => s.startsWith("exit")})
+  val lpwd: PartialFunction[Command, Option[String]] = {
+    case c: Command if c.command == "lpwd" => Option(localFilesystem.workingDirectory)
+  }
+  val lls: PartialFunction[Command, Option[String]] = {
+    case c: Command if c.command == "lls" => Option(localFilesystem.list(c.arguments.headOption))
+  }
+  val lcd: PartialFunction[Command, Option[String]] = {
+    case c: Command if c.command == "lcd" => localFilesystem.changeWorkingDirectory(c.arguments.head); None
+  }
   val commandHandlers = Iterable[PartialFunction[Command, Option[String]]](lpwd, lls, lcd)
 
   def start(): Unit = {
@@ -42,13 +48,12 @@ class Shell {
         result.foreach(println)
       }
     }
-    prompt.eachLine(createShellPrompt) { line =>
-      val command = parseCommand(line)
 
-      command.map { c =>
-        val maybeExecute = executeHandler(c) _
-        commandHandlers.foreach(maybeExecute)
-      }.recover { case e:ParseError => println(e.formatTraces) }
+    prompt.eachLine(createShellPrompt) { line =>
+      parseCommand(line).map { c =>
+        val maybeExecuteHandler = executeHandler(c) _
+        commandHandlers.foreach(maybeExecuteHandler)
+      }.recover { case e: ParseError => println(e.formatTraces)}
     }
   }
 
